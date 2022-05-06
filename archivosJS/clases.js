8b51cdaad82e3ee1303e5a9781ac8587d5bf67f7
delazzer27 <tomas.delazer@gmail.com>
2022-05-05 22:45:43 -0300

    mejorando contacto y filtro y agregando librerias

--- archivosJS/clases.js
+++ archivosJS/clases.js
@@ -9,41 +9,123 @@ class CursoProd {
 
 let listaCursos = [];
 
-const agregarCurso = () => {
-    let materia = document.querySelectorAll(".card-title").value;
-    let docente = document.querySelectorAll(".docente").value;
-    let precio = document.querySelector("#precio").value;
-    let nuevoCurso = new CursoProd(materia, docente, precio);
-    listaCursos.push(nuevoCurso);
-    console.log(nuevoCurso);
-}
+// const agregarCurso = () => {
+//     let materia = document.querySelectorAll(".card-title").value;
+//     let docente = document.querySelectorAll(".docente").value;
+//     let precio = document.querySelector("#precio").value;
+//     let nuevoCurso = new CursoProd(materia, docente, precio);
+//     listaCursos.push(nuevoCurso);
+//     console.log(nuevoCurso);
+// }
 
+// CARRITO 
+$(function (){
 
-// #################FILTRAR POR MATERIA 
+    $("#cart-items").slideUp();
+    $(".cart").on("click", function () {
+    $("#cart-items").slideToggle();
+    });
 
-let boton = document.querySelector("#btnFiltrar");
+    $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
 
-boton.addEventListener("click", (e) => {
+    
+    $(".item").on("click", function () {
+  $("#cart-items").slideDown();
+ setTimeout(function(){
+    $("#cart-items").slideUp();
+ }, 3000)
+        //AGREGAR ITEMS AL CARRITO 
+        $(this).each(function () {
+            let name = $(this).children(".item-detail").children(".card-body").children("h4").text();
+            let remove = "<button class='remove'> X </button>";
+            let cena = "<span class='eachPrice'>" + (parseFloat($(this).children(".item-detail").children(".card-body").children(".prices").children(".price").text())) + "</span>";
+            $("#list-item").append("<li>" + name + "&#09; - &#09;" + cena + "$" + remove + "</li>");
+            listaCursos.push(name)
+            localStorage.setItem("materia", JSON.stringify(listaCursos));
+            //CANTIDAD DE ITEMS 
+            $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
+            $("#items-basket").text();
+    
+        //CALCULAR PRECIO FINAL
+        let totalPrice = 0;
+            $(".eachPrice").each(function (){ 
+              let cenaEach = parseFloat($(this).text());
+              totalPrice+=cenaEach;
+            });
+            $("#total-price").text(totalPrice + "$");
+            // listaCursos.push(name, );
+            // console.log(listaCursos);
+        });
+
+        //SACAR ITEMS
+        $(".remove").on("click", function () {
+            $(this).parent().remove();
+            let totalPrice = 0;
+            $(".eachPrice").each(function (){ 
+              let cenaEach = parseFloat($(this).text());
+              totalPrice+=cenaEach;
+            });
+            $("#total-price").text(totalPrice + "$");
+            $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
+        });
+    });
+})
+
+// #################FILTRAR POR MATERIA, averiguar por el acento al buscar
+
+let boton1 = document.querySelector("#btnFiltrar1");
+let botonAtras1 = document.querySelector("#atras");
+
+boton1.addEventListener("click", (e) => {
             let buscarMateria = document.querySelector("#buscar1").value.toLowerCase();
-            let tituloClase = document.querySelectorAll(".card");
+            let tituloClase = document.querySelectorAll(".productos");
             if (buscarMateria !== "" && buscarMateria !== null && buscarMateria !== undefined) {
-
                 for (const elemento of tituloClase) {
-                    if (!elemento.className.includes(buscarMateria)) {
+                    if (!elemento.innerText.includes(buscarMateria)) {
                         let div = elemento.className.split(" ");
-                        elemento.className = `card ${div[1]} filter`;
+                        elemento.className = `col item productos filter`;
                     }
                 }
             } else {
                 for (const elemento of tituloClase) {
                     let div = elemento.className.split(" ");
-                    elemento.className = `card ${div[1]}`;
+                    elemento.className = `col item productos`;
                 } 
             }
-
-                /* let resultado = tituloClase.find(el => el.innerHTML.includes(buscarMateria)); */
-
             })
+botonAtras1.addEventListener("click", (e) => {
+            let tituloClase = document.querySelectorAll(".productos");
+                for (const elemento of tituloClase) {
+                        let div = elemento.className.split(" ");
+                            elemento.className = `col item productos`;
+                }
+            })
+// #################FILTRAR POR PRECIO , averiguar como hacer para que arroje precios menores al que pone el usuario
 
-// CARRITO 
-const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+let boton2 = document.querySelector("#btnFiltrar2");
+let botonAtras2 = document.querySelector("#atras2");
+boton2.addEventListener("click", (e) => {
+            let tituloClase = document.querySelectorAll(".productos");
+            let buscarPrecio = document.querySelector("#buscar2").value;
+            if (buscarPrecio !== "" && buscarPrecio !== null && buscarPrecio !== undefined) {
+
+                for (const elemento of tituloClase) {
+                    if (!elemento.innerText.includes(buscarPrecio)) {
+                        let div = elemento.className.split(" ");
+                        elemento.className = `col item productos filter`;
+                    }
+                }
+            } else {
+                for (const elemento of tituloClase) {
+                    let div = elemento.className.split(" ");
+                    elemento.className = `col item productos`;
+                } 
+            }
+        })
+        botonAtras2.addEventListener("click", (e) => {
+            let tituloClase = document.querySelectorAll(".productos");
+                for (const elemento of tituloClase) {
+                        let div = elemento.className.split(" ");
+                            elemento.className = `col item productos`;
+                }
+            })
--- archivosJS/contacto.js
+++ archivosJS/contacto.js
@@ -2,39 +2,19 @@ const formulario = document.querySelector("form")
 const boton = document.querySelector (".boton");
 const input = document.querySelectorAll ("input");
 
-// const validarInput = () => {
-//     if (input.value == "") {
-//         input.className = "vacio";
-//     } 
-//     else {
-//         input.className = "validado";
-//     }
-// }
-
-// input.addEventListener ("input", validarInput);
+// No toma el required del HTML 
 boton.onclick = (e) => {
-    // validarInput ();
     e.preventDefault();
     const inputName = document.querySelector('#inputName').value
     const inputTel = document.querySelector('#inputTel').value
     const inputEmail = document.querySelector('#inputEmail').value
-    if (!inputName && !inputTel && !inputEmail ){
+    if (!inputName ||  !inputTel ||  !inputEmail ){
         console.log("falta alguno de los campos")
     } else {
-        alert ("Se envío el formulario correctamente");
+        swal("Se envío el formulario correctamente", "Aceptar", "success");
+        setTimeout (()=>{
+            formulario.submit();
+        }
+        ,3000)
     }
-    formulario.submit();
 }
-
-// const datosFormNombre = document.querySelector("#inputName");
-// datosFormNombre.onchange = () => {
-//     console.log (datosFormNombre.value);
-// }
-// const datosFormTel = document.querySelector("#inputTel");
-// datosFormTel.onchange = () => {
-//     console.log (datosFormTel.value);
-// }
-// const datosFormMail = document.querySelector("#inputEmail");
-// datosFormMail.onchange = () => {
-//     console.log (datosFormMail.value);
-// }
